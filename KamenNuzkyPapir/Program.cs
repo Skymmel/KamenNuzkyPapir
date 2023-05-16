f776debb691cf9e1ab34343b79d03878e333cedd
Skymmel <65237069+Skymmel@users.noreply.github.com>
2023-05-16 12:28:01 +0200

    Initial commit

---
+++ KamenNuzkyPapir/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Spectre.Console;
+
+
+namespace KamenNuzkyPapir
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string nickname = GetNickname();
+            Console.Clear();
+            Random r = new Random();
+
+            int randomSelector = r.Next(0, 3);
+            var fruit = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("What's your [green]favorite fruit[/]?")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .AddChoices(new[] {
+                        "Apple", "Apricot", "Avocado", 
+                        "Banana", "Blackcurrant", "Blueberry",
+                        "Cherry", "Cloudberry", "Cocunut",
+                    }));
+            
+        }
+
+        private static string GetNickname()
+        {
+            AnsiConsole.MarkupLine("What's your name?");
+            AnsiConsole.Markup("My name is ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            return Console.ReadLine()?.Trim();
+        }
+    }
+}
