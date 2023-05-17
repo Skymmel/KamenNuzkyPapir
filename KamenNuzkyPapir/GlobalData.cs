4fffe09f7bf15d8477a62bc4c6a37fb6ff8fe21c
Skymmel <65237069+Skymmel@users.noreply.github.com>
2023-05-17 10:01:18 +0200

    základ přidán

---
+++ KamenNuzkyPapir/GlobalData.cs
@@ -0,0 +1,8 @@
+﻿namespace KamenNuzkyPapir
+{
+    public class GlobalData
+    {
+        public static string[] Weapon = { "Kamen", "Nůžky", "Papír" };
+
+    }
+}
--- KamenNuzkyPapir/Program.cs
+++ KamenNuzkyPapir/Program.cs
@@ -6,25 +6,17 @@ namespace KamenNuzkyPapir
 {
     internal class Program
     {
+        // GlobalData gd = new GlobalData();
+
         public static void Main(string[] args)
         {
-            string nickname = GetNickname();
-            Console.Clear();
-            Random r = new Random();
-
-            int randomSelector = r.Next(0, 3);
-            var fruit = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("What's your [green]favorite fruit[/]?")
-                    .PageSize(10)
-                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-                    .AddChoices(new[] {
-                        "Apple", "Apricot", "Avocado", 
-                        "Banana", "Blackcurrant", "Blueberry",
-                        "Cherry", "Cloudberry", "Cocunut",
-                    }));
-            
+            while (true)
+            {
+                MatchEvaluate(Attack());
+                Console.Clear();
+            }
         }
+        
 
         private static string GetNickname()
         {
@@ -32,6 +24,51 @@ namespace KamenNuzkyPapir
             AnsiConsole.Markup("My name is ");
             Console.ForegroundColor = ConsoleColor.Cyan;
             return Console.ReadLine()?.Trim();
+        }
+
+        private static string Attack()
+        {
+            var offensiveWeapon = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Vyberte věc pro útok:")
+                    .PageSize(4)
+                    .AddChoices(GlobalData.Weapon)
+                );
+            return offensiveWeapon;
+        }
+        private static void PrintStats(byte lives)
+        {
+            for (int i = 0; i < lives; i++)
+            {
+                AnsiConsole.Markup("[red]♥ [/]");
+            }
+        }
+
+        private static void MatchEvaluate(string attack)
+        {
+            Random r = new Random();
+            string cAttack = GlobalData.Weapon[r.Next(0, 3)];
+
+            if (cAttack.Equals(attack))
+            {
+                AnsiConsole.MarkupLine("[yellow]Nerozhodne[/]");
+            }
+            else if
+                (
+                    (attack == "Kamen" && cAttack == "Nůžky")
+                    ||
+                    (attack == "Nůžky" && cAttack == "Papír")
+                    ||
+                    (attack == "Papír" && cAttack == "Kamen")
+                )
+            {
+                AnsiConsole.MarkupLine("[green]Vyhral jsi[/]");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Prohral jsi[/]");
+            }
+            Console.ReadKey();
         }
     }
 }
